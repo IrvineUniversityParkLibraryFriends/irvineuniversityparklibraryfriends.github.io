9c8fb26f448b295d2356931decb3e535939def0f
Zack Hsi <zhsi@lyft.com>
2017-04-12 22:41:20 -0600

    Fix autotrack (#2)
    https://github.com/googleanalytics/autotrack/issues/137

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -25,15 +25,11 @@ if (typeof window !== "undefined") {
   // https://github.com/googleanalytics/autotrack
 
   // most important plugin for phenomic
-  require("autotrack/lib/plugins/url-change-tracker")
   ga("require", "urlChangeTracker")
 
   // some plugins you might like
-  require("autotrack/lib/plugins/clean-url-tracker")
   ga("require", "cleanUrlTracker")
-  require("autotrack/lib/plugins/outbound-form-tracker")
   ga("require", "outboundFormTracker")
-  require("autotrack/lib/plugins/outbound-link-tracker")
   ga("require", "outboundLinkTracker")
 
   // check out more here https://github.com/googleanalytics/autotrack#plugins
@@ -48,6 +44,9 @@ const AppContainer = (props) => (
       scripts={ [
         // GOOGLE Analytics, part 2/2
         { async: true, src: "https://www.google-analytics.com/analytics.js" },
+        // Generated with:
+        //   ./node_modules/autotrack/bin/autotrack -o scripts/autotrack.custom.js -p eventTracker,outboundLinkTracker,urlChangeTracker,outboundFormTracker,cleanUrlTracker
+        { async: true, src: "assets/autotrack.custom.js" },
       ] }
     />
     <Header />
